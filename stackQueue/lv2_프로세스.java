b35333578e4f31a9598e5ffab990e3f074a68248
joongwonAn <anjoongwon517@gmail.com>
2026-06-01 20:52:32 +0900

    [프로그래머스] Queue | Lv2 - 프로세스

---
+++ stackQueue/lv2_프로세스.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new int[]{i, priorities[i]});
+        }
+
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int[] front = queue.poll();
+
+            // Queue에 더 높은 우선순위가 있는지 확인
+            boolean hasHigher = false;
+            for (int[] q : queue) {
+                if (q[1] > front[1]) {
+                    hasHigher = true;
+                    break;
+                }
+            }
+
+            if (hasHigher) {
+                queue.offer(front);  // 다시 뒤로
+            } else {
+                answer++;
+                if (front[0] == location) {
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+// 의사코드
+// 큐(queue)에 [인덱스, 우선순위] 쌍으로 넣기
+// 큐가 빌때까지 반복
+// poll()로 맨 앞을 꺼내서
+// 현재 큐 안에 더 높은 우선순위가 있으면, 다시 offer()
+// 없으면 count++, 꺼낸 것의 인덱스 == location이면 count return
